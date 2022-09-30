47a3138462b81d4a674a8ca56c87f98e8947d7f0
GeniePrinie <geniexpn@gmail.com>
2022-09-30 14:08:29 +0200

    added error message  for textarea

--- js/contact.js
+++ js/contact.js
@@ -3,6 +3,8 @@ const fullName = document.querySelector("#name");
 const nameError = document.querySelector("#nameError");
 const email = document.querySelector("#email");
 const emailError = document.querySelector("#emailError");
+const textMessage = document.querySelector("#textMessage");
+const textMessageError = document.querySelector("#textMessageError");
 const message = document.querySelector(".message");
 
 form.addEventListener("submit", validateTheForm);
@@ -11,13 +13,13 @@ function validateTheForm(event) {
   event.preventDefault();
   message.innerHTML = "";
 
-  if (formValid(fullName, email)) {
+  if (formValid(fullName, email, textMessage)) {
     form.reset();
     message.innerHTML = `<p id="message">Your form has been submitted</p>`;
   }
 }
 
-function formValid(name, email) {
+function formValid(name, email, textMessage) {
   let isValid = true;
 
   if (checkLength(name.value, 1)) {
@@ -31,6 +33,12 @@ function formValid(name, email) {
     emailError.innerHTML = `<p></p>`;
   } else {
     emailError.innerHTML = `<p>Please enter a valid email address</p>`;
+    isValid = false;
+  }
+  if (checkLength(textMessage.value, 1)) {
+    textMessageError.innerHTML = `<p></p>`;
+  } else {
+    textMessageError.innerHTML = `<p>Message is required</p>`;
     isValid = false;
   }
 
